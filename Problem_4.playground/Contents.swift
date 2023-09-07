d8ae022912d9bae23be2b252d1fc21d955986ab4
Utku Guzel <utkugzl@icloud.com>
2023-09-07 22:17:25 +0300

    4th problem solution

--- Problem_4.playground/Contents.swift
+++ Problem_4.playground/Contents.swift
@@ -2,12 +2,14 @@ import UIKit
 
 /*
  
+ PROBLEM-4
+ 
  A palindromic number reads the same both ways. The largest palindrome made from the product of two
  2-digit numbers is 9009 = 91 x 99.
  
  Find the largest palindrome made from the product of two 3-digit numbers.
  
- */
+*/
 
 func isPalindrome(number: Int) -> Bool {
     var firstNumberStr = String(number)
